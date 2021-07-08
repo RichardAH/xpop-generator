ae62b678fad0813376aee46efe24250f0b7cfcc1
Richard Holland <richard.holland@starstone.co.nz>
2021-07-08 12:08:11 +0200

    pov finished, not well tested

--- pov.js
+++ pov.js
@@ -1,86 +1,5 @@
 const crypto = require('crypto')
 
-const sha512h = b => 
-{
-    if (typeof(b) == 'string')
-        b = Buffer.from(b, 'hex')
-    return crypto.createHash('sha512').update(b).digest().slice(0, 32).toString('hex').toUpperCase()
-}
-
-const prefix_LWR = '4C575200'
-const prefix_SND = '534E4400' 
-const prefix_MIN = '4D494E00'
-const prefix_TXN = '54584E00'
-
-const numToHex = (n, size) =>
-{ 
-    if (typeof(n) != 'string')
-        n = n.toString(16) 
-    n = '0'.repeat((size*2)-n.length) + n
-    return n
-}
-
-const compute_ledger_hash = 
-(ledger_index, total_coins,
- parent_hash, transaction_hash, account_hash,
- parent_close_time, close_time, close_time_resolution, close_flags) =>
-{
-    if (typeof(parent_hash) != 'string')
-        parent_hash = parent_hash.toString('hex')
-
-    if (typeof(transaction_hash) != 'string')
-        transaction_hash = transaction_hash.toString('hex')
-
-    if (typeof(account_hash) != 'string')
-        account_hash = account_hash.toString('hex')
-
-    return crypto.createHash('sha512').update(
-        Buffer.from(
-            prefix_LWR +
-            numToHex(ledger_index, 4) +
-            numToHex(total_coins, 8) +
-            parent_hash +
-            transaction_hash +
-            account_hash +
-            numToHex(parent_close_time, 4) +
-            numToHex(close_time, 4) +
-            numToHex(close_time_resolution, 1) +
-            numToHex(close_flags, 2))).digest().slice(0,32).toString('hex').toUpperCase()
-}
-
-const compute_merkle_hash = (tree, depth=0) =>
-{
-    const hex = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 
-                 8:'8', 9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
-
-    const nullhash = '0'.repeat(64)
-
-    let payload = prefix_MIN
-    let hasher = crypto.createHash('sha512')
-    hasher.update(Buffer.from(prefix_MIN, 'hex'))
-    for (let i = 0; i < 16; ++i)
-    {
-        let nibble = hex[i]
-        let to_append = ''
-        if (tree.children[nibble] === undefined)
-            to_append = nullhash
-        else if (Object.keys(tree.children[nibble].children).length == 0)
-            to_append = tree.children[nibble].hash
-        else
-            to_append = compute_merkle_hash(tree.children[nibble], depth+1)
-
-        console.log('  '.repeat(depth) + i, to_append)
-        
-        payload += to_append
-        hasher.update(Buffer.from(to_append, 'hex'))
-    }
-
-//    console.log('payload', payload)
-    tree.hash = hasher.digest().slice(0,32).toString('hex').toUpperCase()
-    //tree.hash = sha512h(payload)
-    return tree.hash
-}
-
 const make_vl_bytes = len =>
 {
     const report_error = e => { console.error(e) }
@@ -125,16 +44,90 @@ const make_vl_bytes = len =>
     }
 }
 
+const sha512h = b => 
+{
+    if (typeof(b) == 'string')
+        b = Buffer.from(b, 'hex')
+    return crypto.createHash('sha512').update(b).digest().slice(0, 32).toString('hex').toUpperCase()
+}
+
+const prefix_LWR = '4C575200'
+const prefix_SND = '534E4400' 
+const prefix_MIN = '4D494E00'
+const prefix_TXN = '54584E00'
+const hex = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 
+             8:'8', 9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+
+const numToHex = (n, size) =>
+{ 
+    if (typeof(n) != 'string')
+        n = n.toString(16) 
+    n = '0'.repeat((size*2)-n.length) + n
+    return n
+}
+
+const hash_ledger = 
+(ledger_index, total_coins,
+ parent_hash, transaction_hash, account_hash,
+ parent_close_time, close_time, close_time_resolution, close_flags) =>
+{
+    if (typeof(parent_hash) != 'string')
+        parent_hash = parent_hash.toString('hex')
+
+    if (typeof(transaction_hash) != 'string')
+        transaction_hash = transaction_hash.toString('hex')
+
+    if (typeof(account_hash) != 'string')
+        account_hash = account_hash.toString('hex')
+
+    return crypto.createHash('sha512').update(
+        Buffer.from(
+            prefix_LWR +
+            numToHex(ledger_index, 4) +
+            numToHex(total_coins, 8) +
+            parent_hash +
+            transaction_hash +
+            account_hash +
+            numToHex(parent_close_time, 4) +
+            numToHex(close_time, 4) +
+            numToHex(close_time_resolution, 1) +
+            numToHex(close_flags, 2))).digest().slice(0,32).toString('hex').toUpperCase()
+}
+
+const compute_tree = (tree, depth=0) =>
+{
+
+    const nullhash = '0'.repeat(64)
+
+    let hasher = crypto.createHash('sha512')
+    hasher.update(Buffer.from(prefix_MIN, 'hex'))
+    for (let i = 0; i < 16; ++i)
+    {
+        let nibble = hex[i]
+        let to_append = ''
+        if (tree.children[nibble] === undefined)
+            to_append = nullhash
+        else if (Object.keys(tree.children[nibble].children).length == 0)
+            to_append = tree.children[nibble].hash
+        else
+            to_append = compute_tree(tree.children[nibble], depth+1)
+
+        hasher.update(Buffer.from(to_append, 'hex'))
+    }
+
+    tree.hash = hasher.digest().slice(0,32).toString('hex').toUpperCase()
+    return tree.hash
+}
+
+
 const hash_txn = txn =>
 {
     if (typeof(txn) != 'string')
         txn = txn.toString('hex')
-    const hash = sha512h(prefix_TXN + txn)
-    console.log('txnhash: ' + hash)
-    return hash
+    return sha512h(prefix_TXN + txn)
 }
 
-const hash_txn_and_meta = (txn,meta) =>
+const hash_txn_and_meta = (txn, meta) =>
 {
     if (typeof(txn) != 'string')
         txn = txn.toString('hex')
@@ -142,14 +135,17 @@ const hash_txn_and_meta = (txn,meta) =>
         meta = meta.toString('hex')
     const vl1 = make_vl_bytes(txn.length/2)
     const vl2 = make_vl_bytes(meta.length/2)
-    const payload = prefix_SND + vl1 + txn + vl2 + meta + hash_txn(txn)
-    console.log('leaf:', payload) 
-    return sha512h(payload)
+    return sha512h(prefix_SND + vl1 + txn + vl2 + meta + hash_txn(txn))
 }
 
-const build_merkle_tree = txns => 
+const report_error = e =>
 {
-    const report_error = e => { console.error(e) }
+    throw(e)
+    //console.error(e)
+}
+
+const create_tree = txns => 
+{
     let root = {children: {}, hash: null, key: '0'.repeat(64)}
 
     // pass one: populate
@@ -159,9 +155,7 @@ const build_merkle_tree = txns =>
         const meta = txns[k].meta
 
         const hash = hash_txn(txn)
-            //hash_txn_and_meta(txn, meta)
 
-        console.log('hash', hash)
         let node = root
         let upto = 0
 
@@ -169,8 +163,6 @@ const build_merkle_tree = txns =>
         while (upto < hash.length)
         {
             let nibble = hash[upto]
-
-            console.log('txnid = ', k, 'nibble', hash.slice(0, upto+1), nibble)
 
             if (!(nibble in node.children))
             {
@@ -209,12 +201,115 @@ const build_merkle_tree = txns =>
     }
 
     // pass two: recursively compute hashes
-    compute_merkle_hash(root)
+    compute_tree(root)
 
     return root
 }
 
-const fs = require('fs')
-const result = JSON.parse(fs.readFileSync('example').toString('utf-8'))
-const txns = result.result.ledger.transactions
-console.log(JSON.stringify(build_merkle_tree(txns), " ", 2))
+// generate the proof
+// pass valid merkle tree and the canonical txn hash as key
+const create_proof_from_tree = (tree, key, upto = 0) =>
+{
+    if (tree === undefined)
+        return false
+
+    tree = tree.children
+   
+    if (tree === undefined)
+        return false
+
+    let proof = []
+
+    let n = parseInt(key[upto], 16)
+
+    for (let i = 0; i < 16; ++i)
+    {
+        const h = hex[i]
+        if (i == n)
+        {
+            if (tree[h] === undefined)
+                return false
+            else if (tree[h].key == key)
+                proof.push(tree[h].hash)
+            else
+            {
+                let retval = create_proof_from_tree(tree[h], key, upto+1)
+                if (!retval)
+                    return false
+                proof.push(retval)
+            }
+        }
+        else if (tree[h] === undefined)
+            proof.push('0'.repeat(64))
+        else
+            proof.push(tree[h].hash)
+    }
+    return proof
+}
+
+const create_proof = (txns, key) =>
+{
+    const tree = create_tree(txns)
+    if (!tree)
+        return false
+    return create_proof_from_tree(tree, key, 0)
+}
+
+const hash_proof = (proof) =>
+{
+    if (proof === undefined)
+        return false
+
+    let hasher = crypto.createHash('sha512')
+    hasher.update(Buffer.from(prefix_MIN, 'hex'))
+    for (let i = 0; i < 16; ++i)
+    {
+        if (proof[i] === undefined)
+            return false
+        else if (typeof(proof[i]) == 'string')
+            hasher.update(Buffer.from(proof[i], 'hex'))
+        else
+            hasher.update(Buffer.from(hash_proof(proof[i]), 'hex'))
+    }
+    return hasher.digest().slice(0,32).toString('hex').toUpperCase()
+}
+
+const verify_proof = (root_hash, proof) =>
+{
+    if (typeof(root_hash) != 'string' || typeof(proof) != 'object')
+        return false
+
+    return root_hash.toUpperCase() == hash_proof(proof)
+}
+
+const proof_contains = (proof, tx_blob, meta, already_computed = false) =>
+{
+    if (proof === undefined)
+        return false
+
+    const hash = (already_computed ? already_computed : hash_txn_and_meta(tx_blob, meta))
+
+    for (let i = 0; i < 16; ++i)
+    {
+        if (proof[i] === undefined)
+            return false
+
+        if (proof[i] == hash)
+            return true
+
+        if (typeof(proof[i]) == 'object' && proof_contains(proof[i], null, null, hash))
+            return true       
+    }
+
+    return false
+}
+
+module.exports = {
+    hash_ledger: hash_ledger,
+    hash_txn: hash_txn,
+    hash_txn_and_meta: hash_txn_and_meta,
+    hash_proof: hash_proof,
+    create_proof: create_proof,
+    verify_proof: verify_proof,
+    proof_contains: proof_contains
+}
